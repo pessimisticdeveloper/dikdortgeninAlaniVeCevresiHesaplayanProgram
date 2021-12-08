98b85030ea7d9558b0d1641530a8754b82f968a9
Mücahit Yılmaz <82089578+pessimisticdeveloper@users.noreply.github.com>
2021-12-08 23:42:26 +0300

    Add files via upload

---
+++ dikdortgeninAlanıVeCevresiHesaplayan.py
@@ -0,0 +1,8 @@
+#Dikdörtgenin alanı ve cevresi hesaplayan
+
+k1 = int(input("kenar1 girin:"))
+k2 = int(input("kenar2 girin:"))
+cevre = 2 * (k1 + k2)
+alan = (k1 * k2)
+print("alan:", alan)
+print("cevre:", cevre)
